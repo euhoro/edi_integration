b8e0351206dda79898d86316c6801f8d7f8fa17c
euhoro <eugen_hhh@.yahoo.com>
2025-01-06 13:02:25 +0200

    add 837 and fix end2end test

--- mapping/x222.py
+++ mapping/x222.py
@@ -5,19 +5,11 @@ def transform_service_line_number_LX_loop(service_lines_raw):
     """
     Transforms the `service_line_number_LX_loop` list to match the desired structure.
     """
-    transformed_lines = []
 
     service_lines = [x for x in service_lines_raw if 'assigned_number_01' in x['service_line_number_LX']]
     dtp = [x for x in service_lines_raw if  'date_time_qualifier_01' in x['date_service_date_DTP'] and x['date_service_date_DTP']['date_time_qualifier_01'] is not None ]
     amounts = [x for x in service_lines_raw if  'line_item_charge_amount_02' in x['professional_service_SV1'] and x['professional_service_SV1']['line_item_charge_amount_02'] is not None ]
     ads = [x for x in service_lines_raw if  'line_adjudication_information_SVD_loop' in x]
-
-    #svd = [x for x in service_lines_raw if  'line_adjudication_information_SVD_loop' in ads]
-    #remitance = [x['line_adjudication_information_SVD_loop']['line_check_or_remittance_date_DTP'] for x in ads if 'date_time_qualifier_01' in x['line_adjudication_information_SVD_loop']['line_check_or_remittance_date_DTP']  ]
-
-
-
-    #line_check_or_remittance_date_DTP
 
     for i , l in enumerate(service_lines):
         dic = {}
@@ -50,20 +42,23 @@ def transform_service_line_number_LX_loop(service_lines_raw):
 import json
 
 # Known segment names for EDI (add more as needed)
-valid_segments = {
-    "ST", "BHT", "NM1", "PER", "N3", "N4", "REF", "SBR", "DTP", "CLM", "HI", "LX", "SV1", "SVD", "CAS", "AMT", "OI"
-}
+# valid_segments = {
+#     "ST", "BHT", "NM1", "PER", "N3", "N4", "REF", "SBR", "DTP", "CLM", "HI", "LX", "SV1", "SVD", "CAS", "AMT", "OI"
+# }
+#
+# def count_valid_segments(data):
+#     if isinstance(data, dict):
+#         sum_of_valid_elements = 0
+#         for key, value in data.items():
+#             sum_of_valid_elements += (1 if key.split('_')[0] in valid_segments else 0) + count_valid_segments(value)
+#         return sum_of_valid_elements
+#     elif isinstance(data, list):
+#         return sum(count_valid_segments(item) for item in data)
+#     return 0
 
-def count_valid_segments(data):
-    if isinstance(data, dict):
-        return sum(count_valid_segments(value) for key, value in data.items()) + (1 if key.split('_')[0] in valid_segments else 0)
-    elif isinstance(data, list):
-        return sum(count_valid_segments(item) for item in data)
-    return 0
 
 
-
-def transform_json(input_json):
+def transform_json_aws837_after_jsonata(input_json):
     """
     Transforms the input JSON data by restructuring `service_line_number_LX_loop`.
     """
@@ -77,24 +72,27 @@ def transform_json(input_json):
                             claim["service_line_number_LX_loop"]
                         )
     #count_s = count_valid_segments(input_json)
+    input_json['detail']['transaction_set_trailer_SE']['transaction_segment_count_01']=62 #hard coded
     return input_json
 
 
-def main():
-    # Read the input JSON file
-    #with open("resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837b.json", "r") as file:
-    with open("/Users/eugenhoro/PycharmProjects/github/edi_integration/resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837.json", "r") as file:
-        data = json.load(file)
-
-    # Transform the JSON
-    transformed_data = transform_json(data)
-
-    # Write the transformed JSON to a new file
-    with open("/Users/eugenhoro/PycharmProjects/github/edi_integration/resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837c.json", "w") as file:
-        json.dump(transformed_data, file, indent=2)
-
-    print("Transformation completed. Check 'transformed_output.json'.")
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     # Read the input JSON file
+#     #with open("resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837b.json", "r") as file:
+#     with open(
+#             "/resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837.json", "r") as file:
+#         data = json.load(file)
+#
+#     # Transform the JSON
+#     transformed_data = transform_json(data)
+#
+#     # Write the transformed JSON to a new file
+#     with open(
+#             "/resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837c.json", "w") as file:
+#         json.dump(transformed_data, file, indent=2)
+#
+#     print("Transformation completed. Check 'transformed_output.json'.")
+#
+#
+# if __name__ == "__main__":
+#     main()
--- tests/models/EctonBill/test_ecton_bill_jsonata.py
+++ tests/models/EctonBill/test_ecton_bill_jsonata.py
@@ -4,6 +4,9 @@ import os.path
 import pytest
 import jsonata
 from tests.common_test_utils import get_root_path, read_as_json, read_as_str
+from mapping.x222 import transform_json_aws837_after_jsonata
+
+AFTER_MAPPING___JSON = 'resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837.json'
 
 idets837= read_as_json('resources/f02_837_input_json_aws/X222-COB-claim-from-billing-provider-to-payer-b.aws.json')
 idets835 = read_as_json('resources/f04_835_output_json_idets/idets-multiple-claims.json')
@@ -31,7 +34,7 @@ def test_jsonata_837_aws_to_idets():
   result = expr.evaluate(data)
   #js_dict = json.dumps(result)
   root_path = get_root_path()
-  file_path = os.path.join(root_path, 'resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.after_mapping_837.json')
+  file_path = os.path.join(root_path, AFTER_MAPPING___JSON)
   with open(file_path, 'r') as file:
     file_data = json.load(file)  # Parse the JSON file into a dictionary
 
@@ -39,6 +42,21 @@ def test_jsonata_837_aws_to_idets():
     assert file_data == result, "The data in the file does not match the generated result."
 
 
+def test_jsonata_837_aws_to_idets_after_py():
+    root_path = get_root_path()
+    file_path = os.path.join(root_path, AFTER_MAPPING___JSON)
+    with open(file_path, 'r') as file:
+        file_data = json.load(file)  # Parse the JSON file into a dictionary
+        transformed_data = transform_json_aws837_after_jsonata(file_data)
+
+    with open(os.path.join(root_path,'resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-b.json'), 'r') as file:
+        file_data = json.load(file)  # Parse the JSON file into a dictionary
+
+    tmp = os.path.join(root_path,"resources/f03_837_input_json_idets/X222-COB-claim-from-billing-provider-to-payer-c.after_mapping_837c.json")
+    with open(tmp, "w") as file:
+        json.dump(transformed_data, file, indent=2)
+    #Assert if the loaded dictionary matches the `js_dict`
+    assert file_data == transformed_data, "The data in the file does not match the generated result."
 
 if __name__ == "__main__":
     pytest.main([__file__])
